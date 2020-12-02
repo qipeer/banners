b45d19ad4599a989eb68c6b827cc67d447902824
qipeer <75220785+qipeer@users.noreply.github.com>
2020-12-02 20:20:49 +0100

    Banners portfolio

---
+++ main.js
@@ -0,0 +1,80 @@
+$('.banner8').click(function() {
+    $('main').css('dilter','blur(5px)');
+    $('.js-overlay-8-campaing').fadeIn();
+    $('.js-overlay-8-campaing').addClass('disabled');
+});
+
+$('.js-close-campaing').click(function(){
+    $('.js-overlay-8-campaing').fadeOut();
+    $('main').css('filter','none');
+});
+$('.banner7').click(function() {
+    $('main').css('dilter','blur(5px)');
+    $('.js-overlay-7-campaing').fadeIn();
+    $('.js-overlay-7-campaing').addClass('disabled');
+});
+
+$('.js-close-campaing').click(function(){
+    $('.js-overlay-7-campaing').fadeOut();
+    $('main').css('filter','none');
+});
+$('.banner6').click(function() {
+    $('main').css('dilter','blur(5px)');
+    $('.js-overlay-6-campaing').fadeIn();
+    $('.js-overlay-6-campaing').addClass('disabled');
+});
+
+$('.js-close-campaing').click(function(){
+    $('.js-overlay-6-campaing').fadeOut();
+    $('main').css('filter','none');
+});
+$('.banner5').click(function() {
+    $('main').css('dilter','blur(5px)');
+    $('.js-overlay-5-campaing').fadeIn();
+    $('.js-overlay-5-campaing').addClass('disabled');
+});
+
+$('.js-close-campaing').click(function(){
+    $('.js-overlay-5-campaing').fadeOut();
+    $('main').css('filter','none');
+});
+$('.banner4').click(function() {
+    $('main').css('dilter','blur(5px)');
+    $('.js-overlay-4-campaing').fadeIn();
+    $('.js-overlay-4-campaing').addClass('disabled');
+});
+
+$('.js-close-campaing').click(function(){
+    $('.js-overlay-4-campaing').fadeOut();
+    $('main').css('filter','none');
+});
+$('.banner3').click(function() {
+    $('main').css('dilter','blur(5px)');
+    $('.js-overlay-3-campaing').fadeIn();
+    $('.js-overlay-3-campaing').addClass('disabled');
+});
+
+$('.js-close-campaing').click(function(){
+    $('.js-overlay-3-campaing').fadeOut();
+    $('main').css('filter','none');
+});
+$('.banner2').click(function() {
+    $('main').css('dilter','blur(5px)');
+    $('.js-overlay-2-campaing').fadeIn();
+    $('.js-overlay-2-campaing').addClass('disabled');
+});
+
+$('.js-close-campaing').click(function(){
+    $('.js-overlay-2-campaing').fadeOut();
+    $('main').css('filter','none');
+});
+$('.banner1').click(function() {
+    $('main').css('dilter','blur(5px)');
+    $('.js-overlay-1-campaing').fadeIn();
+    $('.js-overlay-1-campaing').addClass('disabled');
+});
+
+$('.js-close-campaing').click(function(){
+    $('.js-overlay-1-campaing').fadeOut();
+    $('main').css('filter','none');
+});
